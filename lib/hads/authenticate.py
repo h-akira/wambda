dde0be221bfa53d2d57b44202bb00fdb98ca3b92
h-akira <79441679+h-akira@users.noreply.github.com>
2025-05-01 23:42:18 +0900

    fix

--- lib/hads/authenticate.py
+++ lib/hads/authenticate.py
@@ -44,6 +44,7 @@ class Cognito:
       from jwt import decode
       return decode(id_token, options={"verify_signature": False})
   def _cal_secret_hash(self, username):
+    import base64
     if username is None:
       raise Exception("username is None")
     import hmac
